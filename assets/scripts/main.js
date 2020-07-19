9525797aaa65ac0e345b4814e6acc886c1732ba4
Julián <julianchapela@gmail.com>
2020-07-19 13:36:58 -0300

    avances en front

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,9 @@
-    
+var burger = document.getElementById('burger');
+burger.addEventListener("click",function(){
+    var ul = document.getElementById('lista');
+    if(ul.style.display=="none"){
+        ul.style.display="block"
+    }else{
+        ul.style.display="none";
+    }
+})    
