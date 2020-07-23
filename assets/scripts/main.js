fce7e7e6241e23ab7cd7b98aee9b84e3f60f927f
julianchape <julianchapela@gmail.com>
2020-07-23 12:50:10 -0300

    trending fetch and addToDOM

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,3 +7,24 @@ var burger = document.getElementById('burger');
                 x.style.display = "block";
             }
         })
+
+const apiKey = '2j2nFdUnwzYEf2AFwEXO1LlzWlJFNI5I';
+async function getTrendingGifs(){
+    let url = 'http://api.giphy.com/v1/gifs/trending?api_key='+apiKey+'&limit=3'
+    const trendingData = await fetch (url);
+    const trendingArray = await trendingData.json();
+    return trendingArray.data;
+}
+
+async function addTrendingToDom(){
+    var poner = await getTrendingGifs();
+    var seccion = document.getElementById('trending');
+    console.log(poner);
+    for (let i=0;i<poner.length;i++){
+        var img = document.createElement('img');
+        img.setAttribute('src',poner[i].images.downsized_large.url);
+        seccion.appendChild(img);
+    }
+}
+
+addTrendingToDom();
